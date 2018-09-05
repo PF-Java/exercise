b4c61931becab7187e4406f43f66805492ba3493
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-05 23:42:55 +0700

    pass test

--- src/main/java/NameClassCodeGym.java
+++ src/main/java/NameClassCodeGym.java
@@ -1,8 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NameClassCodeGym {
+    private static final String NAME_CLASS_RERGEC = "^[CAP][0-9]{4}[GHIKLM][0-9]$";
     public NameClassCodeGym() {
     }
 
     public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(NAME_CLASS_RERGEC);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
 
     }
 }
